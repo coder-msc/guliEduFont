97be75eb25fedd94cfbb71523f88dd514f817c81
jkma <1771947201@qq.com>
2020-07-29 00:29:40 +0800

    【代码添加】 封装Axios,显示轮播图

--- src/api/edu/chapter.js
+++ src/api/edu/chapter.js
@@ -1,41 +1,41 @@
 import request from '@/utils/request'
 
 export default{
-  //1 课程信息
+  // 1 课程信息
   getAllChapterVideo(courseId) {
     return request({
-      url:`/eduservice/chapter/getChapterVideo/${courseId}`,
-      method:'get'
+      url: `/eduservice/chapter/getChapterVideo/${courseId}`,
+      method: 'get'
     })
   },
-  //添加章节
-  addChapter(chapter){
+  // 添加章节
+  addChapter(chapter) {
     return request({
-      url:`/eduservice/chapter/addChapter`,
-      method:'post',
-      data:chapter
-})
-  },
-  //修改章节
-  //根据Id查询章节
-  getChapter(chapterId){
-    return request({
-      url:`/eduservice/chapter/getChapterInfo/${chapterId}`,
-      method:'get'
+      url: `/eduservice/chapter/addChapter`,
+      method: 'post',
+      data: chapter
     })
   },
-  updateChapter(chapter){
+  // 修改章节
+  // 根据Id查询章节
+  getChapter(chapterId) {
     return request({
-      url:`/eduservice/chapter/updateChapter`,
-      method:'post',
-      data:chapter
+      url: `/eduservice/chapter/getChapterInfo/${chapterId}`,
+      method: 'get'
     })
   },
-  //删除章节
-  deleteChapter(chapterId){
+  updateChapter(chapter) {
     return request({
-      url:'/eduservice/chapter/'+chapterId,
-      method:'delete'
+      url: `/eduservice/chapter/updateChapter`,
+      method: 'post',
+      data: chapter
     })
   },
+  // 删除章节
+  deleteChapter(chapterId) {
+    return request({
+      url: '/eduservice/chapter/' + chapterId,
+      method: 'delete'
+    })
+  }
 }
